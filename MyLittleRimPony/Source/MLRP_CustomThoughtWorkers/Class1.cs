ae5e45684502fe84c82784f96506e4e8731ba595
GeodesicDragon <chris@curquhart.co.uk>
2022-01-06 20:26:10 +0000

    Version 1.28.92
    Another big update; see Release notes for more information.

---
+++ MyLittleRimPony/Source/MLRP_CustomThoughtWorkers/Class1.cs
@@ -0,0 +1,110 @@
+﻿// Huge thanks to Aelanna from the official RimWorld Discord server for helping me out with this thing.
+// And also for being so damn patient with me while my slow brain figured it all out.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace MyLittleRimPony
+{
+    [DefOf]
+    public static class MyDefOf
+    {
+        public static TraitDef MLRP_BronyTrait;
+        public static TraitDef MLRP_AntiBronyTrait;
+
+        static MyDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(MyDefOf));
+        }
+    }
+
+    // ANTI BRONY STUFF
+
+public class ThoughtWorker_AntiBronyVsBrony : ThoughtWorker
+{
+        protected override ThoughtState CurrentSocialStateInternal(Pawn p, Pawn otherPawn)
+        {
+            if (!p.RaceProps.Humanlike)
+            {
+                return false;
+            }
+            if (!p.story.traits.HasTrait(MyDefOf.MLRP_AntiBronyTrait))
+            {
+                return false;
+            }
+            if (!otherPawn.RaceProps.Humanlike)
+            {
+                return false;
+            }
+            if (!otherPawn.story.traits.HasTrait(MyDefOf.MLRP_BronyTrait))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    public class Thought_AntiBronyVsBrony : Thought_SituationalSocial
+{
+    public override float OpinionOffset()
+    {
+        if (ThoughtUtility.ThoughtNullified(pawn, def))
+        {
+            return 0f;
+        }
+
+        if (otherPawn.story.traits.HasTrait(MyDefOf.MLRP_BronyTrait))
+        {
+            return -50f;
+        }
+            return 0f;
+    }
+}
+
+    // PRO BRONY STUFF
+
+    public class ThoughtWorker_BronyLikesBrony : ThoughtWorker
+    {
+        protected override ThoughtState CurrentSocialStateInternal(Pawn p, Pawn otherPawn)
+        {
+            if (!p.RaceProps.Humanlike)
+            {
+                return false;
+            }
+            if (!p.story.traits.HasTrait(MyDefOf.MLRP_BronyTrait))
+            {
+                return false;
+            }
+            if (!otherPawn.RaceProps.Humanlike)
+            {
+                return false;
+            }
+            if (!otherPawn.story.traits.HasTrait(MyDefOf.MLRP_BronyTrait))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+    public class Thought_BronyLikesBrony : Thought_SituationalSocial
+    {
+        public override float OpinionOffset()
+        {
+            if (ThoughtUtility.ThoughtNullified(pawn, def))
+            {
+                return 0f;
+            }
+
+            if (otherPawn.story.traits.HasTrait(MyDefOf.MLRP_BronyTrait))
+            {
+                return 25f;
+            }
+            return 0f;
+        }
+    }
+}
