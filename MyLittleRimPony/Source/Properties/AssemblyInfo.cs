d1d52f84fefe2546af4f0519bc2c4a0fafc76efe
GeodesicDragon <christopherurquhart2022+github@gmail.com>
2023-10-02 19:32:30 +0100

    Version 4.30.82
    See release.

--- MyLittleRimPony/Source/Properties/AssemblyInfo.cs
+++ MyLittleRimPony/Source/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.26.68.0")]
-[assembly: AssemblyFileVersion("4.26.68.0")]
+[assembly: AssemblyVersion("4.30.82.0")]
+[assembly: AssemblyFileVersion("4.30.82.0")]
 [assembly: NeutralResourcesLanguage("")]
